cadf8f8186085d5abeb31e0996cd3da780c02b01
P Cope <pcope0@gmail.com>
2017-03-16 15:35:35 +0000

    key property added to td elements generated from array

--- src/Row/Row.jsx
+++ src/Row/Row.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Row = props => (
   <tr>
-    {props.items.map((item: number) => <td>{item}</td>)}
+    {props.items.map((item: number) => <td key={item}>{item}</td>)}
   </tr>
 )
 
