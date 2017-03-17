91200b7de5671f81f1f2dca8ff3fe2c80951b331
P Cope <pcope0@gmail.com>
2017-03-17 14:12:08 +0000

    dimensions for grid generation in action creator

--- store/action-creators/generate-grid.js
+++ store/action-creators/generate-grid.js
@@ -2,14 +2,31 @@
 
 import actionTypes from '../action-types.js'
 
-export default (): Object => ({
-    type: actionTypes.INITIALISE_GRID,
-    grid: [[
-            {actualValue: 1, userValue: null, isHidden: false},
-            {actualValue: 2, userValue: null, isHidden: false}
-           ],
-           [
-            {actualValue: 3, userValue: null, isHidden: false},
-            {actualValue: 4, userValue: null, isHidden: false}
-           ]],
-})
+export default (): Object => {
+
+    let grid = []
+
+    const emptyCell = () => ({
+                       actualValue: 1,
+                       userValue: null,
+                       possibleValues: new Set([1,2,3,4,5,6,7,8,9]),
+                       isHidden: false
+                      })
+
+    for (let i = 0; i < 9; i++) {
+
+        let row = []
+
+        for (let j = 0; j < 9; j++) {
+
+            row = [...row, emptyCell()]
+        }
+
+        grid = [...grid, row]
+    }
+
+    return {
+      type: actionTypes.INITIALISE_GRID,
+      grid: grid
+    }
+}
