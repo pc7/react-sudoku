62e55746892e824b1453fe4e0a90376e7b0e8e99
P Cope <pcope0@gmail.com>
2017-03-18 21:56:49 +0000

    grid value generation backtrack algorithm implemented

--- store/action-creators/generateGrid.js
+++ store/action-creators/generateGrid.js
@@ -10,28 +10,39 @@ export default (): Object => {
 
     let grid = emptyGrid(3, 3)
 
-    for (var i = 0, len = grid.length; i < len; i++) {
+    for (var rowIndex = 0, len = grid.length; rowIndex < len; rowIndex++) {
 
-        let row = grid[i]
+        let row = grid[rowIndex]
 
-        for (let j = 0, len = row.length; j < len; j++) {
+        for (let colIndex = 0, len = row.length; colIndex < len; colIndex++) {
 
             const sameDomainCellValues : Set<?cellValue> = new Set(
-              [...findSameDomainCells(grid, i, j)]
+              [...findSameDomainCells(grid, rowIndex, colIndex)]
                 .map((cell : Cell) : ?cellValue => cell.actualValue)
                 .filter((value : ?cellValue) : boolean => value !== null)
             )
 
-            const nonConflictingPossibleValues : Set<cellValue> = new Set([...row[j].possibleValues].filter(x => !sameDomainCellValues.has(x)))
+            const nonConflictingPossibleValues : Set<cellValue> = new Set([...row[colIndex].possibleValues].filter(x => !sameDomainCellValues.has(x)))
 
             if (nonConflictingPossibleValues.size) {
-                //console.log(nonConflictingPossibleValues)
-                //row[j].actualValue = [...nonConflictingPossibleValues][0]  // TODO: randomise this.
-                // remove selected value from possibleValues
+
+                const value = [...nonConflictingPossibleValues][Math.floor(Math.random() * nonConflictingPossibleValues.size)]
+                row[colIndex].actualValue = value
+                row[colIndex].possibleValues.delete(value)
             } else {
-                // set cell.actualValue to null
-                // reset cell.possibleValues to [1..9]
-                // backtrack to previous cell
+
+                row[colIndex].actualValue = null
+                row[colIndex].possibleValues = new Set([1,2,3,4,5,6,7,8,9])
+
+                if (colIndex !== 0) {
+                    // If the current square is not at the end of its row, jump back to the previous square.
+                    colIndex = colIndex - 2
+                } else {
+                    // Else, jump back to the last square on the previous row.
+                    colIndex = grid[rowIndex].length-1
+                    rowIndex = rowIndex - 2
+                    break
+                }
             }            
         }
     }
