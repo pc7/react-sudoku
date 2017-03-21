8f80e6b9fd648c2ceded677ecbe1b31b1da9e717
P Cope <pcope0@gmail.com>
2017-03-21 16:24:10 +0000

    basic CellSelect click event handler implemented

--- src/CellSelect/CellSelect.jsx
+++ src/CellSelect/CellSelect.jsx
@@ -2,6 +2,9 @@
 
 import React from 'react'
 import { connect } from 'react-redux'
+import sameDomainCellValues from '../../store/grid-utils/same-domain-cell-values/sameDomainCellValues.js'
+import validCellValues from '../../store/grid-utils/validCellValues.js'
+import diffSet from '../../utils/diffSet.js'
 import type { Cell as CellType, cellValue } from '../../store/grid-utils/types.js'
 
 const CellSelect : Function = React.createClass({
@@ -12,12 +15,28 @@ const CellSelect : Function = React.createClass({
   //               and assigned to Component state.
 
   handleSelectClick() {
-    // assign possibleUserValues to Component state.
-    //this.props.initialiseGrid()
+
+    var colIndex : number = -1;
+
+    for (var rowIndex : number = 0, len = this.props.grid.length; rowIndex < len; rowIndex++) {
+        colIndex = this.props.grid[rowIndex].indexOf(this.props.cellRef)
+        if (colIndex !== -1) break
+    }
+
+    console.log(rowIndex)
+    console.log(colIndex)
+
+    const nonConflictingPossibleUserValues : Set<cellValue> = diffSet(
+      new Set(validCellValues()),
+      sameDomainCellValues(this.props.grid, rowIndex, colIndex, 'userValue')
+    )
+
+    console.log(nonConflictingPossibleUserValues)
+
+    this.setState({possibleUserValues: [...nonConflictingPossibleUserValues]})
   },
 
   handleOptionClick() {
-    //this.props.initialiseGrid()
   },
 
   render() {
@@ -25,12 +44,11 @@ const CellSelect : Function = React.createClass({
     return (
       <select name="possibleUserValues" onClick={(e) => this.handleSelectClick(e)}>
         <option value={null}>None</option>
+        {this.state.possibleUserValues.map((value: cellValue) =>
+          <option key={value} value={value}>{value}</option>
+         )}
       </select>
     )
-    // other option elements...
-    //{this.state.possibleUserValues.map((value: cellValue) =>
-    //  <option key={value} value={value}>{value}</option>
-    // )}
   }
 })
 
