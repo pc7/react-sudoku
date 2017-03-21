b7749eb23690491da06cda92a57b698197dd25fe
P Cope <pcope0@gmail.com>
2017-03-21 15:19:40 +0000

    CellSelect component changed to being connected, ready to dynamically generate possible user values from application state

--- src/CellSelect/CellSelect.jsx
+++ src/CellSelect/CellSelect.jsx
@@ -1,20 +1,50 @@
 // @flow
 
 import React from 'react'
+import { connect } from 'react-redux'
 import type { Cell as CellType, cellValue } from '../../store/grid-utils/types.js'
 
-const CellSelect : Function = (props : {cellRef: CellType}) => (
-  <select name="possibleUserValues">
-    <option value={null}>None</option>
-  </select>
-)
+const CellSelect : Function = React.createClass({
+
+  // ARCHITECTURE: possibleUserValues is derived from application state, and is not part of
+  //               application state, ie it does not affect external Components.
+  //               So, possible user values should be generated dynamically when needed
+  //               and assigned to Component state.
+
+  handleSelectClick() {
+    // assign possibleUserValues to Component state.
+    //this.props.initialiseGrid()
+  },
+
+  handleOptionClick() {
+    //this.props.initialiseGrid()
+  },
+
+  render() {
+
+    return (
+      <select name="possibleUserValues" onClick={(e) => this.handleSelectClick(e)}>
+        <option value={null}>None</option>
+      </select>
+    )
     // other option elements...
-    //{props.cellRef.possibleUserValues.map((value: cellValue) =>
+    //{this.state.possibleUserValues.map((value: cellValue) =>
     //  <option key={value} value={value}>{value}</option>
     // )}
+  }
+})
 
 CellSelect.propTypes = {
   cellRef: React.PropTypes.object,
 }
 
-export default CellSelect
+const mapStateToProps = (storeState: Object): Object => ({
+  grid: storeState.grid
+})
+
+const mapDispatchToProps = (dispatch: Function): Object => ({
+//  initialiseGrid: () => dispatch(generateGrid())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CellSelect)
+
--- store/action-creators/generateGrid.js
+++ store/action-creators/generateGrid.js
@@ -7,6 +7,9 @@ import setHiddenCells from '../grid-utils/set-hidden-cells/setHiddenCells.js'
 
 import type { action_initialiseGrid } from '../grid-utils/types.js'
 
+// TODO: Remove possibleActualValues and possibleUserValues properties from cell objects
+//       before assigning to store. possibleActualValues is used during grid generation,
+//       and is not part of application state.
 export default () : action_initialiseGrid => ({
   type: actionTypes.INITIALISE_GRID,
   grid: setHiddenCells(setActualValues(emptyGrid(3, 3)), 5)
