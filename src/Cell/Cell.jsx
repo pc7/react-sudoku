a9fb58cfdcb278815d8736f821b6868db65119ad
P Cope <pcope0@gmail.com>
2017-03-26 16:43:04 +0100

    basic modular css for grid

--- src/Cell/Cell.jsx
+++ src/Cell/Cell.jsx
@@ -2,10 +2,11 @@
 
 import React from 'react'
 import CellSelect from '../CellSelect/CellSelect.jsx'
+import styles from './Cell.css'
 import type { Cell as CellType } from '../../store/grid-utils/types.js'
 
 const Cell : Function = (props : {item: CellType}) => (
-  <td>
+  <td className={styles.cell + ' ' + (!props.item.isHidden ? styles.revealed : '')}>
     {!props.item.isHidden ? props.item.actualValue : props.item.userValue}
     {!props.item.isHidden ? null : <CellSelect cellRef={props.item}/>}
   </td>
--- src/Row/Row.jsx
+++ src/Row/Row.jsx
@@ -2,10 +2,11 @@
 
 import React from 'react'
 import Cell from '../Cell/Cell.jsx'
+import styles from './Row.css'
 import type { Cell as CellType } from '../../store/grid-utils/types.js'
 
 const Row : Function = (props : {items: CellType[]}) => (
-  <tr>
+  <tr className={styles.row}>
     {props.items.map((item: CellType) =>
        <Cell item={item} key={item.actualValue} />
      )}
